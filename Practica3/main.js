26e1e5424c88e800e9240fd99808d913363f03c8
MaeZam <glezmae6@gmail.com>
2025-05-22 19:01:54 -0600

    Ejercicio A
    Importación de la función restar

---
+++ Practica3/main.js
@@ -0,0 +1,5 @@
+//importaciín de la función restar con su ruta
+import { restar } from './utils.js';
+
+console.log(restar(5, 3)); 
+console.log(restar(100, 80)); 
---
+++ Practica3/utils.js
@@ -0,0 +1,8 @@
+
+//Definición de la función restar
+function restar(a, b) { //parametros de la función a y b
+    return a - b;
+}
+
+// Exportar la función para poder usarla en otros archivos
+export { restar };
