6d79dc591a99395c22278964c0c7ea7c36936ff1
MaeZam <glezmae6@gmail.com>
2025-06-19 18:22:16 -0600

    práctica 9
    uso del TextInput

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,110 +1,65 @@
-// Switch
-//Zona 1: importaciones
-import { StatusBar } from 'expo-status-bar';
+//zona 1: importaciones
 import { useState } from 'react';
-import { StyleSheet, Text, View, Switch } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-// import React,{useState} from 'react' ; 
+import { StyleSheet, Text, View, TextInput, Button, Alert,  } from 'react-native';
 
-//Zona 2: Main - ejecución
+//zona 2: main(Ejecución)
 export default function App() {
 
-  //Activar y desactivar el modo oscuro
-  //El valor por defecto es false, lo que significa que el modo oscuro está desactivado
-  //Al cambiar el estado a true, se activa el modo oscuro
-  const [activarSwitch, setActivarSwitch] = useState(false);
-  //Estado para el modo oscuro 
-  const [modoOscuro, setModoOscuro] = useState(false);
+  const [nombre, setNombre] = useState('');
+
+  const mostrarAlerta = () => {
+    if (nombre.trim()=== ''){
+      Alert.alert('Error', 'Por favor escribe algo');
+      alert('Escribe algo');
+    }
+    else{
+      Alert.alert('Bienvenido ', `Hola ${nombre} ,  bienvenido a nuestras app`);
+      alert('Hola ' + nombre + ' Bienvenido')
+    }
+    
+  }
 
   return (
-    <SafeAreaProvider>
-       <SafeAreaView style={[styles.contenedor, modoOscuro && styles.fondoOscuro]}>
-        
-        {/*Se van a agregar más componentes */}
-        <Text style={[styles.titulo, modoOscuro && styles.textoClaro]}>
-          Práctica con Switches
-        </Text>
+    <View style={styles.container}>
 
-        <View style={styles.opcion}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}>
-            Activar Switch 2
-          </Text>
-          {/* //Componente Switch que activa y desactiva el switch del modo oscuro */}
-          <Switch 
-            // {/*Estilos y funcionalidad del Switch */}
-            value={activarSwitch}
-            onValueChange={setActivarSwitch}
-            // {/* El valor del Switch se actualiza con el estado activarSwitch */}
-            trackColor={{false:'#ccc', true:'#4caf50'}}
-            // {/* Color del cambio del switch */}
-            thumbColor={activarSwitch ? '#ffffff' : '#999999'}>
-          </Switch>
-        </View>
+      <Text style={styles.text}> Ingresa tu nombre </Text>
+      <TextInput 
+      style={styles.input} 
+      placeholder='Escribe tu nombre'
+      onChangeText={setNombre}
+      value={nombre}
+      >
+      </TextInput>
 
-        <View style={styles.opcion}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}>
-            Modo Oscuro
-          </Text>
-          
-          <Switch 
-            // {/*Estilos y funcionalidad del Switch */} {/* El valor del Switch se actualiza con el estado modoOscuro */}
-            value={modoOscuro} 
-            // {/* El switch solo se puede activar si el switch de arriba está activado */}
-            disabled={!activarSwitch} 
-            onValueChange={setModoOscuro}
-            trackColor={
-              !activarSwitch
-                // Si el switch de arriba está desactivado, el color del track es gris
-                ? {false:'#999999', true:'#ff3b30'}
-                : {false: '#ccc', true: '#4caf50'}
-            }
-            // {/* Color del cambio del switch */}
-            thumbColor={
-              // Si el switch de arriba está desactivado, el color del thumb es gris
-              !activarSwitch
-                ? '#ff3b30' // Si el switch de arriba está desactivado, el color del thumb es rojo
-                : modoOscuro // Si el modo oscuro está activado, el color del thumb es blanco
-                  ? '#ffffff'
-                  : '#999999'
-            }>
-          </Switch>
-        </View>
-        
-        </SafeAreaView>
-    </SafeAreaProvider>
-             
+      <Button title='Enviar' onPress={mostrarAlerta}></Button>
+      
+    </View>
   );
 }
 
-//zona 3: Estilos
+//Zona 3: Estilos
 const styles = StyleSheet.create({
- 
-  contenedor: {
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingHorizontal: 30,
+    backgroundColor: 'white',
+    padding : 20,
     justifyContent: 'center',
   },
 
-  titulo: {
-    fontSize: 24,
-    marginBottom: 40,
-    textAlign: 'center',
-    fontWeight: 'bold',
+  //texto
+  text:{
+    marginBottom : 20,
+    fontSize : 18,
+    color : '#000',
+
   },
-  fondoOscuro: {
-    backgroundColor: "#1a1a1a"
+  input : {
+    borderWidth : 1,
+    borderColor : '#ccc',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    backgroundColor : '#f9f9f9',
+    color : '#000',
   },
-  textoClaro: {
-    color: '#ffffff'
-  },
-  opcion: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 30,
-    alignItems: 'center',
-  },
-  etiqueta: {
-    fontSize: 18,
-  }
 });
