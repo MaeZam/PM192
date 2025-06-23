5045c006c70d1aecb0731d0344d22d46f7e21552
MaeZam <glezmae6@gmail.com>
2025-06-23 15:01:11 -0600

    ImageBackground - P10
    CREACIÓN DE COMPONENTE PARA IMPLEMENTAR IMAGENES

--- practica10/App.js
+++ practica10/App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
+
+const FondoBienvenida = () => {
+return (
+<ImageBackground source={require('./assets/hello.png')} style= {styles.fondo}>
+<View>
+<Text  style={styles.titulo} >¡Bienvenido a la App!</Text>
+</View>
+</ImageBackground>
+);
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FondoBienvenida />
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  fondo: {
+    flex: 1,
+  },
+  contenido: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  titulo: {
+    fontSize: 28,
+    color: 'darkblue',
+    fontWeight: 'bold',
   },
 });
